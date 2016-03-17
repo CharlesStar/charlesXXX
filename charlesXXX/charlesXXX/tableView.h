eb2b0a9d66d7caea4116b4763eaad7a344ac3a74
lanou3g <420210267@qq.com>
2016-03-17 09:51:33 +0800

    第二次提交

---
+++ charlesXXX/charlesXXX/tableView.h
@@ -0,0 +1,13 @@
+//
+//  tableView.h
+//  charlesXXX
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 Charles刘星池. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tableView : UITableView
+
+@end
---
+++ charlesXXX/charlesXXX/tableView.m
@@ -0,0 +1,21 @@
+//
+//  tableView.m
+//  charlesXXX
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 Charles刘星池. All rights reserved.
+//
+
+#import "tableView.h"
+
+@implementation tableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
